51e5c3c02e6b1045b242cbda85fc35a729a732c9
Alysson Daniel Nascimento <alyssondaniel@gmail.com>
2017-12-19 11:14:05 -0300

    Styling with React Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Clock from './Clock'
 import './App.css'
+import { Form, FormControl, Button } from 'react-bootstrap'
+
 
 class App extends Component {
   constructor(props) {
@@ -21,10 +23,10 @@ class App extends Component {
       <div className='App'>
         <div className='App-title'>Countdown to {this.state.deadline}</div>
         <Clock deadline={this.state.deadline} />
-        <div>
-          <input placeholder='new data' onChange={event => this.setState({newDeadline: event.target.value})} />
-          <button onClick={() => this.changeDeadline()}>Submit</button>
-        </div>
+        <Form inline>
+          <FormControl className='Deadline-input' placeholder='new data' onChange={event => this.setState({newDeadline: event.target.value})} />
+          <Button onClick={() => this.changeDeadline()}>Submit</Button>
+        </Form>
       </div>
     )
   }
